161e4e09fc6c8ada54eb508da55e790d590a8df1
Rahul George <rahuljithu911@gmail.com>
2024-08-28 23:32:32 +0530

    productcard and productdetails component added

---
+++ array-archers_042/src/components/SidebarNav.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
+const SidebarNav = () => {
+  return (
+    <Box bg="#303030" p={4} width="100%">
+      <Flex justify="start" align="center">
+        <Link to="/bikes">
+          <Text fontSize="lg" color="white" mr={4}>
+            Bikes
+          </Text>
+        </Link>
+        <Link to="/helmets">
+          <Text fontSize="lg" color="white" mr={4}>
+            Helmets
+          </Text>
+        </Link>
+        <Link to="/jackets">
+          <Text fontSize="lg" color="white" mr={4}>
+            Jackets
+          </Text>
+        </Link>
+        <Link to="/gloves">
+          <Text fontSize="lg" color="white">
+            Gloves
+          </Text>
+        </Link>
+      </Flex>
+    </Box>
+  );
+};
+
+export default SidebarNav;
