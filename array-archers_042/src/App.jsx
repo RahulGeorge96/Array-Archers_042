b5589b73b280f538985239f06ff7aaf33b0b4caa
Vikram Kumar Choudhary <vikram0104irctc@gmail.com>
2024-08-28 23:33:34 +0530

    Added the Navbar

--- array-archers_042/src/App.jsx
+++ array-archers_042/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { NavBar } from './components/navBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar/>
     </>
   )
 }
